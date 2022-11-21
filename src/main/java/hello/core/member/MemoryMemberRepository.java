108094dfda91eff3abfc23f90210488e26427f84
ohjiyeon3275 <ojy940716@gmail.com>
2022-11-21 23:24:11 +0900

    강의 정리

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,20 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    // cuncurrency 이슈가 생길수 있으니 연습용으로만 쓰도록
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
