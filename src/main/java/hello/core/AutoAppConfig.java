8422e502a64e86e729540965c470bf06cfd4add3
ohjiyeon3275 <ojy940716@gmail.com>
2022-11-25 22:58:26 +0900

    컴포넌트 스캔 - filter

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.FilterType;
  * @ComponentScan의 쓰임
  */
 @ComponentScan(
-        basePackages ="hello.core.member", // 범위 지정 가능 , 지정하지 않으면 (default는) 
+        basePackages ="hello.core.member", // 범위 지정 가능 , 지정하지 않으면 (default는)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
