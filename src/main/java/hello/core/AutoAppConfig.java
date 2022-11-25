e06197f30e305ce2741cd959ebbdd89047611d63
ohjiyeon3275 <ojy940716@gmail.com>
2022-11-25 23:15:15 +0900

    중복 등록했을때 자동 bean과 수동 bean

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,10 +12,24 @@ import org.springframework.context.annotation.FilterType;
  * @ComponentScan의 쓰임
  */
 @ComponentScan(
-        basePackages ="hello.core.member", // 범위 지정 가능 , 지정하지 않으면 (default는)
+        // basePackages ="hello.core.member", // 범위 지정 가능 , 지정하지 않으면 (default는)
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
 
+    /**
+     * 여기서 MemoryMemberRepository가 두번 생성되는데 (자동 -> @Component, 수동 -> 아래와 같은 방식)
+     *
+     * 테스트 돌려보면 수동 빈이 자동 빈을 오버라이딩한다.
+     * but, App 을 돌렸을때는 에러가 날수있으므로
+     * 프로퍼티에
+     * spring.main.allow-bean-definition-overriding=true
+     * 설정을 해줘야함ㄴ다.
+     */
+
+    @Bean(name="memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
 }
