0b06ceb9718de198727d1649ceaddebd0dfeb635
ohjiyeon3275 <ojy940716@gmail.com>
2022-11-26 14:30:11 +0900

    bean lifecycle 진행중ㅎ

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,31 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+
+        // A.C를 그냥쓰지 않고 Configurable하게 이걸써줌
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient () {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://");
+            return networkClient;
+        }
+    }
+}
