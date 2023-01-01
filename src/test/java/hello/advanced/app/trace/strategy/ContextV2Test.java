36b61cd4a5e8a55ae93eb5d23b84be1e91a1a482
ohjiyeon3275 <ojy940716@gmail.com>
2023-01-01 21:40:01 +0900

    전략패턴의 예시: 익명내부클래스 사용하는 방법

--- src/test/java/hello/advanced/app/trace/strategy/ContextV2Test.java
+++ src/test/java/hello/advanced/app/trace/strategy/ContextV2Test.java
@@ -1,5 +1,6 @@
 package hello.advanced.app.trace.strategy;
 
+import hello.advanced.app.trace.strategy.code.Strategy;
 import org.junit.jupiter.api.Test;
 
 public class ContextV2Test {
@@ -9,5 +10,23 @@ public class ContextV2Test {
         ContextV2 context = new ContextV2();
         context.execute(new StrategeLogic1());
         context.execute(new StrategeLogic2());
+    }
+
+    @Test
+    void strategyV2() {
+        ContextV2 context = new ContextV2();
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                System.out.println("business logic 1 execute");
+            }
+        });
+
+        context.execute(new Strategy() {
+            @Override
+            public void call() {
+                System.out.println("business logic 2 execute");
+            }
+        });
     }
 }
