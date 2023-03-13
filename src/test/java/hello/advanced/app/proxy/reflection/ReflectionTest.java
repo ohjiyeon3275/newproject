7756f86e713e8caa739d2e852ced2a318b94d47a
ohjiyeon3275 <ojy940716@gmail.com>
2023-03-13 23:19:08 +0900

    reflection : 클래스 메타정보와 실제 인스턴스를 결합하여 호출

--- src/test/java/hello/advanced/app/proxy/reflection/ReflectionTest.java
+++ src/test/java/hello/advanced/app/proxy/reflection/ReflectionTest.java
@@ -2,6 +2,8 @@ package hello.advanced.app.proxy.reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class ReflectionTest {
 
     @Test
@@ -32,5 +34,31 @@ public class ReflectionTest {
     /**
      * spring-basic 브랜치에 있는 component-scan기능에 스프링에서 지원하는 리플렉션을 사용한 적 있다.
      * 리플렉션은 클래스나 메서드의 메타정보를 사용해서 동적으로 호출하는 메서드 변경이다.
+     */
+
+    @Test
+    void reflection2() throws Exception {
+
+        Class reflection = Class.forName("hello.advanced.app.proxy.reflection.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+
+        Method methodCallA = reflection.getMethod("callA");
+        Object result1 = methodCallA.invoke(target); // target의 callA를 호출
+        System.out.println("result1 : " + result1);
+
+        Method methodCallB = reflection.getMethod("callB");
+        Object result2 = methodCallB.invoke(target);
+        System.out.println("result2 : " + result2);
+    }
+
+    /**
+     *  1. reflection으로 class를 불러오고
+     *  2. 직접 target을 불러와서
+     *  3. 1의 정보의 메서드를 2의 정보의 class에 실행시킨다.
+     *
+     *  리플렉션해 가져온 클래스 | 직접 가져온 클래스로 나눈 이유는
+     *  클래스나 메서드 정보를 동적으로 변경가능해지기 때문
+     *  또한 공통로직도 만들 수 있다.
      */
 }
